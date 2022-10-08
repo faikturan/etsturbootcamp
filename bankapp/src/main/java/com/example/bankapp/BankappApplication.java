efaf9583b6aa86ff84ddad827ed9a1b1aeec8d8b
Faik Turan <turanfaik@hotmail.com>
2022-10-08 10:31:54 +0300

    Bank App continue

--- bankapp/src/main/java/com/example/bankapp/BankappApplication.java
+++ bankapp/src/main/java/com/example/bankapp/BankappApplication.java
@@ -1,13 +1,21 @@
 package com.example.bankapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-public class BankappApplication {
+@EnableCaching
+public class BankappApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BankappApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
--- bankapp/src/main/java/com/example/bankapp/repository/CitiesRepository.java
+++ bankapp/src/main/java/com/example/bankapp/repository/CitiesRepository.java
@@ -3,6 +3,8 @@ package com.example.bankapp.repository;
 import com.example.bankapp.model.Cities;
 import com.example.bankapp.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+@EnableRedisRepositories
 public interface CitiesRepository extends JpaRepository<Cities, Long> {
 }
